fd9726e312cffa9e8d380136b1f54ba2ef42632c
shim506 <shim506@naver.com>
2022-05-15 17:44:22 +0900

    feat : navigation 제작
    temp : picture

--- app/src/main/java/com/example/starbucks/MainActivity.kt
+++ app/src/main/java/com/example/starbucks/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.starbucks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.starbucks.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigation,
+            findNavController(R.id.nav_host)
+        )
+        binding.bottomNavigation.setupWithNavController(findNavController(R.id.nav_host))
     }
 }
