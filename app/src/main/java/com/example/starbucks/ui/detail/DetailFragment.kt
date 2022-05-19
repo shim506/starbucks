be6567e2001151638f84ff2cfab025981306ec09
shim506 <shim506@naver.com>
2022-05-19 17:49:17 +0900

    temp

--- app/src/main/java/com/example/starbucks/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/starbucks/ui/detail/DetailFragment.kt
@@ -1,4 +1,4 @@
-package com.example.starbucks.ui
+package com.example.starbucks.ui.detail
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -12,23 +12,11 @@ import com.example.starbucks.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class DetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class DetailFragment : Fragment() {
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
